1f61bea5f83a493d3ed8368b3d9f9298ca3bfd50
Mauricio <maumg1196@gmail>
2016-01-22 16:53:00 -0600

    Views

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from blog.models import Post
+from django.utils import timezone
 
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published=timezone.now()).order_by('published')
+    return render(request, 'blog/post_list.html', {'posts': posts})
